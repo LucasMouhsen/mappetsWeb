7033d5613a9298188e50377d37acc6d2c0fc0b6c
lucasmouhsen <lucasmouhsen@gmail.com>
2026-03-20 15:47:07 -0300

    arreglo puerto

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const VITE_API_BASE_URL = "http://api.mappets.com.ar/";
+export const VITE_API_BASE_URL = "http://api.mappets.com.ar:3000";
 
 export const API_BASE_URL = VITE_API_BASE_URL.replace(/\/$/, "");
